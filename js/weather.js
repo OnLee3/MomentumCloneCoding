ea7bbe9836b7fb5e91ec2ec54c590e53edf99db7
OnLee3 <dlwotjr2@gmail.com>
2021-06-13 21:52:26 +0900

    +'s' to align github

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY="ed878c4c12458724e9bfb0c3b163ce3a";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(response => response.json())
     .then(data => {
